4697c284adf914f86ffa801f334cad72460c7b8a
Aiden Carpenter <amctyg@umsystem.edu>
2022-10-25 14:19:20 -0500

    refactor board script

--- Assets/Scripts/Board/Board.cs
+++ Assets/Scripts/Board/Board.cs
@@ -16,150 +16,76 @@ public class Board : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        InstantiateTiles();
+        
+        AutoplaceTiles();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void InstantiateTiles()
+    {
         tiles = new List<GameObject>();
 
+        for (int i = 0; i < NUM_TILES; i++)
+        {
+            GameObject tileObj = new GameObject("tile-" + i, typeof(BoxCollider), typeof(BasicTile), typeof(SpriteRenderer));
+            tileObj.GetComponent<BasicTile>().index = i;
+            tiles.Add(tileObj);
+        }
+
+        tiles[0].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Money/100");
+        tiles[1].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Board/brown space");
+        tiles[2].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Money/500");
+        tiles[3].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Board/brown space");
+        tiles[4].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Money/500");
+        tiles[5].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Money/500");
+        tiles[6].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Board/light blue space");
+        tiles[7].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Money/500");
+        tiles[8].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Board/light blue space");
+        tiles[9].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Board/light blue space");
+        tiles[10].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Money/100");
+        tiles[12].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Board/purple space");
+        tiles[12].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Money/500");
+        tiles[13].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Board/purple space");
+        tiles[14].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Board/purple space");
+        tiles[15].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Money/500");
+        tiles[16].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Board/orange space");
+        tiles[17].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Money/500");
+        tiles[18].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Board/orange space");
+        tiles[19].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Board/orange space");
+        tiles[20].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Money/100");
+        tiles[21].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Board/red space");
+        tiles[22].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Money/500");
+        tiles[23].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Board/red space");
+        tiles[24].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Board/red space");
+        tiles[25].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Money/500");
+        tiles[26].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Board/yellow space");
+        tiles[27].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Board/yellow space");
+        tiles[28].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Money/500");
+        tiles[29].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Board/yellow space");
+        tiles[30].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Money/100");
+        tiles[31].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Board/green space");
+        tiles[32].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Board/green space");
+        tiles[33].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Money/500");
+        tiles[34].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Board/green space");
+        tiles[35].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Money/500");
+        tiles[36].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Money/500");
+        tiles[37].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Board/dark blue space");
+        tiles[38].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Money/500");
+        tiles[39].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Board/dark blue space");
+    }
+
+    private void AutoplaceTiles()
+    {
         sprite = this.GetComponent<SpriteRenderer>().sprite;
         Rect boardRect = sprite.textureRect;
         BOARD_WIDTH_P = boardRect.width;
         BOARD_HEIGHT_P = boardRect.height;
-
-        // TODO: this is temporary, it just adds tiles and gives their sprites and indexes, we need to add each tile individually, so as to customize them, and figure out how to stratify tile charicteristics
-        for (int i = 0; i < NUM_TILES; i++)
-        {
-            GameObject tileObj = new GameObject("tile-" + i, typeof(BoxCollider), typeof(BasicTile), typeof(SpriteRenderer));
-            
-            switch (i)
-            {
-                case 0:
-                    tileObj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Money/100");
-                    break;
-                case 1:
-                    tileObj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Board/brown space");
-                    break;
-                case 2:
-                    tileObj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Money/500");
-                    break;
-                case 3:
-                    tileObj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Board/brown space");
-                    break;
-                case 4:
-                    tileObj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Money/500");
-                    break;
-                case 5:
-                    tileObj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Money/500");
-                    break;
-                case 6:
-                    tileObj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Board/light blue space");
-                    break;
-                case 7:
-                    tileObj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Money/500");
-                    break;
-                case 8:
-                    tileObj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Board/light blue space");
-                    break;
-                case 9:
-                    tileObj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Board/light blue space");
-                    break;
-                case 10:
-                    tileObj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Money/100");
-                    break;
-                case 11:
-                    tileObj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Board/purple space");
-                    break;
-                case 12:
-                    tileObj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Money/500");
-                    break;
-                case 13:
-                    tileObj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Board/purple space");
-                    break;
-                case 14:
-                    tileObj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Board/purple space");
-                    break;
-                case 15:
-                    tileObj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Money/500");
-                    break;
-                case 16:
-                    tileObj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Board/orange space");
-                    break;
-                case 17:
-                    tileObj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Money/500");
-                    break;
-                case 18:
-                    tileObj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Board/orange space");
-                    break;
-                case 19:
-                    tileObj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Board/orange space");
-                    break;
-                case 20:
-                    tileObj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Money/100");
-                    break;
-                case 21:
-                    tileObj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Board/red space");
-                    break;
-                case 22:
-                    tileObj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Money/500");
-                    break;
-                case 23:
-                    tileObj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Board/red space");
-                    break;
-                case 24:
-                    tileObj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Board/red space");
-                    break;
-                case 25:
-                    tileObj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Money/500");
-                    break;
-                case 26:
-                    tileObj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Board/yellow space");
-                    break;
-                case 27:
-                    tileObj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Board/yellow space");
-                    break;
-                case 28:
-                    tileObj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Money/500");
-                    break;
-                case 29:
-                    tileObj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Board/yellow space");
-                    break;
-                case 30:
-                    tileObj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Money/100");
-                    break;
-                case 31:
-                    tileObj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Board/green space");
-                    break;
-                case 32:
-                    tileObj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Board/green space");
-                    break;
-                case 33:
-                    tileObj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Money/500");
-                    break;
-                case 34:
-                    tileObj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Board/green space");
-                    break;
-                case 35:
-                    tileObj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Money/500");
-                    break;
-                case 36:
-                    tileObj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Money/500");
-                    break;
-                case 37:
-                    tileObj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Board/dark blue space");
-                    break;
-                case 38:
-                    tileObj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Money/500");
-                    break;
-                case 39:
-                    tileObj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Board/dark blue space");
-                    break;
-                default:
-                    Debug.Log("We in here");
-                    Debug.LogError("There shouldn't be more than 40 tiles created");
-                    break;
-            }
-            Debug.Log(i);
-            tileObj.GetComponent<BasicTile>().index = i;
-            tiles.Add(tileObj);
-        }
-        
 
         /*
          *  This foreach loop places each tile around the board in the respective place for all tiles with indexes 0-35, starting at the bottom right and working clockwise
@@ -207,11 +133,5 @@ public class Board : MonoBehaviour
             tile.transform.localScale = new Vector3(BOARD_WIDTH_P * transform.localScale.x / (tileRect.width * 11) * unitDisplayRatio, BOARD_HEIGHT_P * transform.localScale.y / (tileRect.height * 11) * unitDisplayRatio, 1f);
             tile.transform.position = new Vector3(transform.position.x + xOffset, transform.position.y + yOffset, 0f);
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
